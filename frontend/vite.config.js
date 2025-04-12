c003e450bac0c875b4cad1f97b553f710d66ae01
smoggydesire <vborcila10@gmail.com>
2025-04-12 11:52:00 +0300

    Update Vite config for CommonJS modules

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    },
     commonjsOptions: {
       include: [/react-slick/, /slick-carousel/, /react/, /react-dom/],
       transformMixedEsModules: true
