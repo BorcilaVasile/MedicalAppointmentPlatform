984053320e46421890e8ce51fcf3b139f82b3861
smoggydesire <vborcila10@gmail.com>
2025-04-13 21:43:25 +0300

    Modified tailwind.config.js to tailwind.config.cjs to be compatible with Vercel

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const daisyui = require('daisyui');
+
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,7 +9,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [(await import("daisyui")).default],
+  plugins: [daisyui],
   daisyui: {
     themes: ["light", "dark"],
   },
